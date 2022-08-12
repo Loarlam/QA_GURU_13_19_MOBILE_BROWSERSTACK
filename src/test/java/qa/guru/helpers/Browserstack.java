968af1ab452fd1eb40026e58cb2d7920a7a7513a
Loarlam <Loarlam@ya.ru>
2022-08-12 11:30:39 +0300

    Добавлены креденшиалы. Скопированы дефолтные тесты.

---
+++ src/test/java/qa/guru/helpers/Browserstack.java
@@ -0,0 +1,21 @@
+package qa.guru.helpers;
+
+import static io.restassured.RestAssured.given;
+import static java.lang.String.format;
+
+public class Browserstack {
+    public static String videoUrl(String sessionId) {
+        String url = format("https://api.browserstack.com/app-automate/sessions/%s.json", sessionId);
+
+        return given()
+                .auth().basic("slishkom_r9HD7d", "fSpGJgENnExScJZTTFaX")
+                .log().all()
+                .when()
+                .get(url)
+                .then()
+                .log().all()
+                .statusCode(200)
+                .extract()
+                .path("automation_session.video_url");
+    }
+}
